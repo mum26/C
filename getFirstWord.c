e804af7a9d52d891e40d0bff8cc64d63c140ea46
mum26 <96600854+mum26@users.noreply.github.com>
2024-01-26 22:49:43 +0900

    Update getFirstWord.c

--- getFirstWord.c
+++ getFirstWord.c
@@ -7,9 +7,9 @@ int getFirstWord(char *, char *);
 
 int main(void) {
     /* getFirstWord関数に値を渡す変数, 受け取る変数を宣言 */
-    char str[] = "abc de fgh";
+    char str[] = "abc de        Hello,World!!";
     char result[128] = "\0";
-    getFirstWord(result, &str[1]);
+    getFirstWord(result, &str[6]);
     printf("%s\n", result);
 }
 
@@ -17,14 +17,13 @@ int main(void) {
 int getFirstWord(char *result, char *str) {
     /* result を初期化し, 受け取ったポインタが NULL なら終了 */
     result[0] = '\0';
-    if (str == NULL) return 1;
+    if (str == NULL) return -1;
 
     /* スペースを飛ばす */
-    int i = 0;
+    int i = 0, j = 0;
     while (str[i] == ' ') i++;
 
     /* スペース or 終端文字 まで文字列を result に代入 */
-    int j = 0;
     while (str[i] != ' ' && str[i] != '\0') result[j++] = str[i++];
 
     /* 終端文字を代入 */
