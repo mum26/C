8d3b7c20b27a7507d48aa1562efbe3c8a96720d8
mum26 <96600854+mum26@users.noreply.github.com>
2024-01-28 20:00:37 +0900

    Update getNextWord.c

--- getNextWord.c
+++ getNextWord.c
@@ -1,10 +1,12 @@
+#include <string.h>
+
 /* 受け取った文字列から次のスペースまでの文字列を変数に戻す */
 int getNextWord(char *result, char *str) {
     /* 受け取ったポインタが NULL なら終了 */
     if (str == NULL) return -1;
 
     /* スペースを飛ばす */
-    int i = 0, j = 0;
+    int i = 0, j = 0, length = 0;
     while (str[i] == ' ') i++;
 
     /* スペース or 終端文字 まで文字列を result に代入 */
@@ -13,5 +15,9 @@ int getNextWord(char *result, char *str) {
     /* 終端文字を代入 */
     result[j] = '\0';
 
-    return 0;
+    /* 文字数を取得 */
+    length = strlen(result);
+    
+    /* 文字数を返す */
+    return length;
 }
