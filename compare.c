f29bbcdb2ab4297fa5754a43015b7af9b1c961f5
mum26 <96600854+mum26@users.noreply.github.com>
2024-02-02 15:52:00 +0900

    Update compare.c

--- compare.c
+++ compare.c
@@ -5,8 +5,8 @@ int compareIntsASC(const void *a, const void *b) {
     int arg1 = *(const int *)a;
     int arg2 = *(const int *)b;
 
-    if (arg1 < arg2) return -1; // arg1がarg2より大きい場合、正の値を返す
-    if (arg1 > arg2) return 1;  // arg1がarg2より小さい場合、負の値を返す
+    if (arg1 > arg2) return 1;  // arg1がarg2より大きい場合、正の値を返す
+    if (arg1 < arg2) return -1; // arg1がarg2より小さい場合、負の値を返す
     return 0;                  // 両者が等しい場合、0を返す
 }
 
